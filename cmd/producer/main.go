0b97279362c021263d4dd5692cf6347dbc900678
Ives M. W <ivesmw@gmail.com>
2024-11-07 15:57:40 -0300

    feat: add delivery reporting for kafka message publisher

--- cmd/producer/main.go
+++ cmd/producer/main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
 )
 
 func main() {
+	deliveryChan := make(chan ckafka.Event)
+
 	producer := NewKafkaProducer()
-	Publish("Mensagem 1", "golang-kafka", producer, nil)
+	Publish("Mensagem 1", "golang-kafka", producer, deliveryChan, nil)
+
+	go DeliveryReport(deliveryChan)
+
 	producer.Flush(1000)
 }
 
@@ -24,17 +30,30 @@ func NewKafkaProducer() *ckafka.Producer {
 	return p
 }
 
-func Publish(msg string, topic string, producer *ckafka.Producer, key []byte) error {
+func Publish(msg string, topic string, producer *ckafka.Producer, deliveryChan chan ckafka.Event, key []byte) error {
 	message := &ckafka.Message{
 		Value:          []byte(msg),
 		TopicPartition: ckafka.TopicPartition{Topic: &topic, Partition: ckafka.PartitionAny},
 		Key:            key,
 	}
 
-	err := producer.Produce(message, nil)
+	err := producer.Produce(message, deliveryChan)
 	if err != nil {
 		return err
 	}
 
 	return nil
+}
+
+func DeliveryReport(deliveryChan chan ckafka.Event) {
+	for e := range deliveryChan {
+		switch ev := e.(type) {
+		case *ckafka.Message:
+			if ev.TopicPartition.Error != nil {
+				fmt.Println("Delivery failed: ", ev.TopicPartition)
+			} else {
+				fmt.Println("Delivered message to: ", ev.TopicPartition)
+			}
+		}
+	}
 }
